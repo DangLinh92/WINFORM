2a14763a77e75835448d59f78673539120304280
danglv <danglevan.9919@gmail.com>
2021-11-16 15:46:18 +0700

    update report

--- ACCOUNT_REPORT/Source/WISOL.UI/Forms/CONTENT/FRM_FINACING_STATEMENT.cs
+++ ACCOUNT_REPORT/Source/WISOL.UI/Forms/CONTENT/FRM_FINACING_STATEMENT.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -34,7 +35,29 @@ namespace Wisol.MES.Forms.CONTENT
 
         private void btnDownloadFile_Click(object sender, EventArgs e)
         {
+            try
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
 
+                saveFileDialog1.Filter = "excel files (*.xlsx)|*.xlsx";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.RestoreDirectory = true;
+                saveFileDialog1.FileName = "Financing Statement-" + DateTime.Parse(dateLoad.EditValue.NullString()).ToString("yyyyMMdd");
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = saveFileDialog1.FileName;
+
+                    // Save the modified document to a file.
+                    IWorkbook workbook = spreadsheetMain.Document;
+                    workbook.SaveDocument(fileName, DocumentFormat.Xlsx);
+                    Process.Start(fileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MsgBox.Show(ex.Message, MsgType.Error);
+            }
         }
 
         private void LoadTemplate()
@@ -64,35 +87,141 @@ namespace Wisol.MES.Forms.CONTENT
 
                     sheet.Cells["A3"].Value = DateTime.Parse(createDate).Year + "년  " + DateTime.Parse(createDate).Month + "월  " + DateTime.Parse(createDate).Day + "일";
 
+                    int startOnHand = 10;
+                    int endOnHand = startOnHand + 2;
+
                     // tien mat
                     if (data[0].Rows.Count > 0)
                     {
-                        if(data[0].Rows.Count > 2)
+                        if (data[0].Rows.Count > 2)
                         {
                             for (int i = 2; i < data[0].Rows.Count; i++)
                             {
-                                sheet.Rows[i + 9].Insert(InsertCellsMode.EntireRow);
-                                //sheet.Rows[i + 9].CopyFrom(sheet.Rows[10], PasteSpecial.Formats);
+                                sheet.Rows[i + startOnHand - 1].Insert(InsertCellsMode.EntireRow);
                             }
                         }
-
-                        sheet.DataBindings.BindToDataSource(data[0], 10, 0);
-                    }
-
-                    int startCashVND = 16;
-                    if(data[0].Rows.Count > 2)
-                    {
-                        startCashVND = 16 + (data[0].Rows.Count - 2);
-                    }
-
-                    sheet.DataBindings.BindToDataSource(data[1], startCashVND, 0);
-                    if (data[1].Rows.Count > 2)
-                    {
-                        for (int i = 2; i < data[1].Rows.Count; i++)
+                        else if (data[0].Rows.Count == 1)
                         {
-                            sheet.Rows[i + 9].Insert(InsertCellsMode.EntireRow);
-                            //sheet.Rows[i + 9].CopyFrom(sheet.Rows[10], PasteSpecial.Formats);
+                            sheet.Rows[startOnHand + 1].Delete();
                         }
+
+                        sheet.DataBindings.BindToDataSource(data[0], startOnHand, 0);
+                        endOnHand = startOnHand + data[0].Rows.Count;
+                    }
+                    else
+                    {
+                        sheet.Rows[startOnHand + 1].Delete();
+                        endOnHand = startOnHand + 1;
+                    }
+
+                    // tien gui ngan hang vnd
+                    int startCashVND = endOnHand + 4;
+                    int endCashVND = startCashVND + 2;
+
+                    if (data[1].Rows.Count > 0)
+                    {
+                        if (data[1].Rows.Count > 2)
+                        {
+                            for (int i = 2; i < data[1].Rows.Count; i++)
+                            {
+                                sheet.Rows[startCashVND + i - 1].Insert(InsertCellsMode.EntireRow);
+                            }
+                        }
+                        else if (data[1].Rows.Count == 1)
+                        {
+                            sheet.Rows[startCashVND + 1].Delete();
+                        }
+
+                        sheet.DataBindings.BindToDataSource(data[1], startCashVND, 0);
+                        endCashVND = startCashVND + data[1].Rows.Count;
+                    }
+                    else
+                    {
+                        sheet.Rows[startCashVND + 1].Delete();
+                        endCashVND = startCashVND + 1;
+                    }
+
+                    // tien gui ngan hang usd
+                    int startCashUsd = endCashVND + 4;
+                    int endCashUsd = startCashUsd + 2;
+
+                    if (data[2].Rows.Count > 0)
+                    {
+                        if (data[2].Rows.Count > 2)
+                        {
+                            for (int i = 2; i < data[2].Rows.Count; i++)
+                            {
+                                sheet.Rows[startCashUsd + i - 1].Insert(InsertCellsMode.EntireRow);
+                            }
+                        }
+                        else if (data[2].Rows.Count == 1)
+                        {
+                            sheet.Rows[startCashUsd + 1].Delete();
+                        }
+
+                        sheet.DataBindings.BindToDataSource(data[2], startCashUsd, 0);
+
+                        endCashUsd = startCashUsd + data[2].Rows.Count;
+                    }
+                    else
+                    {
+                        sheet.Rows[startCashUsd + 1].Delete();
+                        endCashUsd = startCashUsd + 1;
+                    }
+
+                    // Bao Lanh Dien
+                    int startElectric = endCashUsd + 4;
+                    int endElectric = startElectric + 2;
+
+                    if (data[3].Rows.Count > 0)
+                    {
+                        if (data[3].Rows.Count > 2)
+                        {
+                            for (int i = 2; i < data[3].Rows.Count; i++)
+                            {
+                                sheet.Rows[startElectric + i - 1].Insert(InsertCellsMode.EntireRow);
+                            }
+                        }
+                        else if (data[3].Rows.Count == 1)
+                        {
+                            sheet.Rows[startElectric + 1].Delete();
+                        }
+
+                        sheet.DataBindings.BindToDataSource(data[3], startElectric, 0);
+
+                        endElectric = startElectric + data[3].Rows.Count;
+                    }
+                    else
+                    {
+                        sheet.Rows[startElectric + 1].Delete();
+                        endElectric = startElectric + 1;
+                    }
+
+                    // Bao cao vay
+                    int startLoan = endElectric + 4;
+                    int endLoan = startLoan + 2;
+
+                    if (data[4].Rows.Count > 0)
+                    {
+                        if (data[4].Rows.Count > 2)
+                        {
+                            for (int i = 2; i < data[4].Rows.Count; i++)
+                            {
+                                sheet.Rows[startLoan + i - 1].Insert(InsertCellsMode.EntireRow);
+                            }
+                        }
+                        else if(data[4].Rows.Count == 1)
+                        {
+                            sheet.Rows[startLoan + 1].Delete();
+                        }
+
+                        sheet.DataBindings.BindToDataSource(data[4], startLoan, 0);
+                        endLoan = startLoan + data[4].Rows.Count;
+                    }
+                    else
+                    {
+                        sheet.Rows[startLoan + 1].Delete();
+                        endLoan = startLoan + 1;
                     }
                 }
                 else
