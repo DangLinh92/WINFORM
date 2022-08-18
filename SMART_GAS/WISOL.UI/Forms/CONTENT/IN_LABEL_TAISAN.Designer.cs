693a351f378bcd269d374300f1408f95db1b06de
danglv <danglevan.9919@gmail.com>
2022-08-18 11:28:52 +0700

    update for smart gas: in lable ma tai san

---
+++ SMART_GAS/WISOL.UI/Forms/CONTENT/IN_LABEL_TAISAN.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Wisol.MES.Forms.CONTENT
+{
+    partial class IN_LABEL_TAISAN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.xLayoutControl1 = new Wisol.XLayoutControl();
+            this.btnGetFIle = new Wisol.XSimpleButton(this.components);
+            this.btnImport = new Wisol.XSimpleButton(this.components);
+            this.btnPrintTS = new Wisol.XSimpleButton(this.components);
+            this.gcList = new Wisol.XGridControl();
+            this.gvList = new Wisol.XGridView();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.cboPrinter = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.xLayoutControl1)).BeginInit();
+            this.xLayoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gcList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cboPrinter.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.xLayoutControl1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(1039, 597);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "In mã tài sản";
+            // 
+            // xLayoutControl1
+            // 
+            this.xLayoutControl1.Controls.Add(this.cboPrinter);
+            this.xLayoutControl1.Controls.Add(this.btnGetFIle);
+            this.xLayoutControl1.Controls.Add(this.btnImport);
+            this.xLayoutControl1.Controls.Add(this.btnPrintTS);
+            this.xLayoutControl1.Controls.Add(this.gcList);
+            this.xLayoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xLayoutControl1.Location = new System.Drawing.Point(2, 23);
+            this.xLayoutControl1.Name = "xLayoutControl1";
+            this.xLayoutControl1.Root = this.Root;
+            this.xLayoutControl1.Size = new System.Drawing.Size(1035, 572);
+            this.xLayoutControl1.TabIndex = 0;
+            this.xLayoutControl1.Text = "xLayoutControl1";
+            // 
+            // btnGetFIle
+            // 
+            this.btnGetFIle.FormId = null;
+            this.btnGetFIle.isFormType = false;
+            this.btnGetFIle.Location = new System.Drawing.Point(278, 12);
+            this.btnGetFIle.MaximumSize = new System.Drawing.Size(129, 30);
+            this.btnGetFIle.MinimumSize = new System.Drawing.Size(129, 30);
+            this.btnGetFIle.Name = "btnGetFIle";
+            this.btnGetFIle.Size = new System.Drawing.Size(129, 30);
+            this.btnGetFIle.StyleController = this.xLayoutControl1;
+            this.btnGetFIle.TabIndex = 7;
+            this.btnGetFIle.Text = "Lấy file mẫu";
+            this.btnGetFIle.Click += new System.EventHandler(this.btnGetFIle_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.FormId = null;
+            this.btnImport.isFormType = false;
+            this.btnImport.Location = new System.Drawing.Point(12, 12);
+            this.btnImport.MaximumSize = new System.Drawing.Size(129, 30);
+            this.btnImport.MinimumSize = new System.Drawing.Size(129, 30);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(129, 30);
+            this.btnImport.StyleController = this.xLayoutControl1;
+            this.btnImport.TabIndex = 6;
+            this.btnImport.Text = "Import ";
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnPrintTS
+            // 
+            this.btnPrintTS.FormId = null;
+            this.btnPrintTS.isFormType = false;
+            this.btnPrintTS.Location = new System.Drawing.Point(145, 12);
+            this.btnPrintTS.MaximumSize = new System.Drawing.Size(129, 30);
+            this.btnPrintTS.MinimumSize = new System.Drawing.Size(129, 30);
+            this.btnPrintTS.Name = "btnPrintTS";
+            this.btnPrintTS.Size = new System.Drawing.Size(129, 30);
+            this.btnPrintTS.StyleController = this.xLayoutControl1;
+            this.btnPrintTS.TabIndex = 5;
+            this.btnPrintTS.Text = "In label";
+            this.btnPrintTS.Click += new System.EventHandler(this.btnPrintTS_Click);
+            // 
+            // gcList
+            // 
+            this.gcList.Location = new System.Drawing.Point(12, 46);
+            this.gcList.MainView = this.gvList;
+            this.gcList.Name = "gcList";
+            this.gcList.Size = new System.Drawing.Size(1011, 514);
+            this.gcList.TabIndex = 4;
+            this.gcList.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvList});
+            // 
+            // gvList
+            // 
+            this.gvList.GridControl = this.gcList;
+            this.gvList.Name = "gvList";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4,
+            this.layoutControlItem5});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(1035, 572);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.gcList;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 34);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(1015, 518);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.btnPrintTS;
+            this.layoutControlItem2.Location = new System.Drawing.Point(133, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(133, 34);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.btnImport;
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(133, 34);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.btnGetFIle;
+            this.layoutControlItem4.Location = new System.Drawing.Point(266, 0);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(133, 34);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // cboPrinter
+            // 
+            this.cboPrinter.Location = new System.Drawing.Point(457, 12);
+            this.cboPrinter.Name = "cboPrinter";
+            this.cboPrinter.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cboPrinter.Size = new System.Drawing.Size(566, 20);
+            this.cboPrinter.StyleController = this.xLayoutControl1;
+            this.cboPrinter.TabIndex = 8;
+            this.cboPrinter.SelectedIndexChanged += new System.EventHandler(this.cboPrinter_SelectedIndexChanged);
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.cboPrinter;
+            this.layoutControlItem5.Location = new System.Drawing.Point(399, 0);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(616, 34);
+            this.layoutControlItem5.Text = "May IN";
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(34, 13);
+            // 
+            // IN_LABEL_TAISAN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupControl1);
+            this.Name = "IN_LABEL_TAISAN";
+            this.Size = new System.Drawing.Size(1039, 597);
+            this.Controls.SetChildIndex(this.groupControl1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.xLayoutControl1)).EndInit();
+            this.xLayoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gcList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cboPrinter.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private XLayoutControl xLayoutControl1;
+        private XGridControl gcList;
+        private XGridView gvList;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private XSimpleButton btnImport;
+        private XSimpleButton btnPrintTS;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private XSimpleButton btnGetFIle;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraEditors.ComboBoxEdit cboPrinter;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+    }
+}
---
+++ SMART_GAS/WISOL.UI/Forms/CONTENT/IN_LABEL_TAISAN.cs
@@ -0,0 +1,239 @@
+﻿using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Wisol.Common;
+using Wisol.Components;
+using Wisol.MES.Classes;
+using Wisol.MES.Inherit;
+
+namespace Wisol.MES.Forms.CONTENT
+{
+    public partial class IN_LABEL_TAISAN : PageType
+    {
+        public IN_LABEL_TAISAN()
+        {
+            InitializeComponent();
+            this.Load += IN_LABEL_TAISAN_Load;
+        }
+        DataTable Data;
+        private void IN_LABEL_TAISAN_Load(object sender, EventArgs e)
+        {
+            Data = new DataTable();
+            Classes.Common.SetFormIdToButton(this, "IN_LABEL_TAISAN");
+            Classes.Common.SelectPrinter(cboPrinter);
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            //string name file;
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Excel Files (.xls*)|*.xls*|All Files (*.*)|*.*";
+            dlg.Multiselect = false;
+
+            DialogResult dlgResult = dlg.ShowDialog();
+            if (dlgResult == DialogResult.OK)
+            {
+                string filePath = dlg.FileName;
+                string excelcon;
+                if (filePath.Substring(filePath.LastIndexOf('.')).ToLower() == ".xlsx")
+                {
+                    excelcon = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0;HDR=NO;IMEX=1'";
+                }
+                else
+                {
+                    excelcon = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1'";
+                }
+                OleDbConnection conexcel = new OleDbConnection(excelcon);
+
+                try
+                {
+                    conexcel.Open();
+                    DataTable dtExcel = conexcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                    string sheetName = "data$";
+
+                    OleDbCommand cmdexcel1 = new OleDbCommand();
+                    cmdexcel1.Connection = conexcel;
+                    cmdexcel1.CommandText = "select * from[" + sheetName + "]";
+
+                    Data = new DataTable();
+                    OleDbDataAdapter da = new OleDbDataAdapter();
+                    da.SelectCommand = cmdexcel1;
+                    da.Fill(Data);
+                    conexcel.Close();
+                    Data.Rows.RemoveAt(0);
+                    base.m_BindData.BindGridView(gcList, Data);
+                    //gvList.DeleteRow(0);
+                }
+                catch (Exception ex)
+                {
+                    conexcel.Close();
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void btnPrintTS_Click(object sender, EventArgs e)
+        {
+            if (Data.Rows.Count == 0)
+            {
+                return;
+            }
+
+            #region print
+            string designFile = string.Empty;
+            string xml_content_Original = string.Empty;
+            string xml_content = label;
+            try
+            {
+                designFile = "STOCK_LABEL.xml";
+                XtraReport reportPrint = new XtraReport();
+
+                ReportPrintTool pt1 = new ReportPrintTool(reportPrint);
+                pt1.PrintingSystem.StartPrint += new PrintDocumentEventHandler(PrintingSystem_StartPrint);
+
+                List<XtraReport> reports = new List<XtraReport>();
+
+                foreach (DataRow row in Data.Rows)
+                {
+                    xml_content = xml_content.Replace("$CODE$", row[0].NullString()).Replace("$BARCODE$", row[0].NullString()).Replace("$QUANTITY$", "");
+                    xml_content = xml_content.Replace("&", "&amp;");
+                    File.WriteAllText(designFile, xml_content);
+
+                    XtraReport report = new XtraReport();
+                    report.PrintingSystem.ShowPrintStatusDialog = false;
+                    report.PrintingSystem.ShowMarginsWarning = false;
+                    report.LoadLayoutFromXml(designFile);
+
+                    int leftMargine = report.Margins.Left + 0;
+                    int rightMargine = report.Margins.Right;
+                    int topMargine = report.Margins.Top + 0;
+                    int bottomMargine = report.Margins.Bottom;
+                    if (leftMargine < 0)
+                    {
+                        leftMargine = 0;
+                    }
+                    if (topMargine < 0)
+                    {
+                        topMargine = 0;
+                    }
+                    report.Margins = new Margins(leftMargine, rightMargine, topMargine, bottomMargine);
+                    report.CreateDocument();
+
+                    reports.Add(report);
+                    File.Delete(designFile);
+                }
+
+                foreach (XtraReport rp in reports)
+                {
+                    ReportPrintTool pts = new ReportPrintTool(rp);
+                    pts.PrintingSystem.StartPrint += new PrintDocumentEventHandler(reportsStartPrintEventHandler);
+                }
+
+                pt1.PrintDialog();
+                foreach (XtraReport rport in reports)
+                {
+                    ReportPrintTool pts = new ReportPrintTool(rport);
+                    pts.Print();
+                }
+            }
+            catch (Exception ex)
+            {
+                MsgBox.Show(ex.Message, MsgType.Error);
+            }
+            #endregion
+        }
+
+        private PrinterSettings prnSettings;
+        private void reportsStartPrintEventHandler(object sender, PrintDocumentEventArgs e)
+        {
+            int pageCount = e.PrintDocument.PrinterSettings.ToPage;
+            e.PrintDocument.PrinterSettings = prnSettings;
+
+            // The following line is required if the number of pages for each report varies, 
+            // and you consistently need to print all pages.
+            e.PrintDocument.PrinterSettings.ToPage = pageCount;
+        }
+
+        private void PrintingSystem_StartPrint(object sender, PrintDocumentEventArgs e)
+        {
+            prnSettings = e.PrintDocument.PrinterSettings;
+        }
+
+
+
+        private void btnGetFIle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileName = "MataisanTmp.xlsx";
+
+                string url = Application.StartupPath + @"\\" + fileName;
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+                saveFileDialog.FileName = fileName;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (WebClient web1 = new WebClient())
+                        web1.DownloadFile(url, saveFileDialog.FileName);
+                    MsgBox.Show("Thành công!", MsgType.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MsgBox.Show(ex.Message, MsgType.Error);
+            }
+        }
+
+        private void cboPrinter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PrinterClass.SetDefaultPrinter(cboPrinter.Text.Trim());
+            GetLabelTemplate();
+        }
+
+        string label;
+        private void GetLabelTemplate()
+        {
+            try
+            {
+                label = string.Empty;
+                base.m_ResultDB = base.m_DBaccess.ExcuteProc("PKG_BUSINESS_LABEL.GET_TEMP", new string[] { }, new string[] { });//QRCODE 
+                if (m_ResultDB.ReturnInt == 0)
+                {
+                    if (base.m_ResultDB.ReturnDataSet.Tables[0].Rows.Count > 0)
+                    {
+                        label = base.m_ResultDB.ReturnDataSet.Tables[0].Rows[0]["CONTENT_LABEL"].NullString();
+                    }
+                    else
+                    {
+                        MsgBox.Show("Không có File label cho printer " + cboPrinter.Text, MsgType.Warning);
+                    }
+                }
+                else
+                {
+                    MsgBox.Show(m_ResultDB.ReturnString.Translation(), MsgType.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MsgBox.Show(ex.Message, MsgType.Error);
+            }
+        }
+    }
+}
